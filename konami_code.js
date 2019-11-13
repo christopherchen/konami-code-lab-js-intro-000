2d161ab5486aaea6a6a9fca4bc15821f64c4284d
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-13 21:04:27 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,9 +11,13 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
   document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
     
+    if (key === codes[i])
   });
 }
